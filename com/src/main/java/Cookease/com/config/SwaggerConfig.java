7615b1a4dbdd830b91c505f911ebb97c2270af1e
roganyc <roganyc@naver.com>
2024-11-02 20:56:23 +0900

    [FEAT] Swagger 초기 구현

--- com/src/main/java/Cookease/com/config/SwaggerConfig.java
+++ com/src/main/java/Cookease/com/config/SwaggerConfig.java
@@ -1,4 +1,18 @@
 package Cookease.com.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CookEase API")
+                        .description("Search My Recipe by Ingredients in Refrigerator API")
+                        .version("1.0.0"));
+    }
 }
