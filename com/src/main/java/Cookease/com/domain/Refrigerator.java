dd30f9bf861280d8e9e916a3c59db58495c2b28b
Chaeah <147984765+blue850@users.noreply.github.com>
2024-11-02 20:03:12 +0900

    FEAT : Entity 정의

--- com/src/main/java/Cookease/com/domain/Refrigerator.java
+++ com/src/main/java/Cookease/com/domain/Refrigerator.java
@@ -1,4 +1,17 @@
 package Cookease.com.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class Refrigerator {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member member;
+
 }
--- com/src/main/java/Cookease/com/domain/RefrigeratorIngredient.java
+++ com/src/main/java/Cookease/com/domain/RefrigeratorIngredient.java
@@ -1,4 +1,28 @@
 package Cookease.com.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
 public class RefrigeratorIngredient {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="refrigerator_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Refrigerator refrigerator;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="ingredient_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Ingredient ingredient;
+
+    @Column
+    private LocalDateTime expirationDate;
 }
