e6beaf97b47c840aa3eb8374263ea255244ebd4b
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-11-03 04:47:01 +0900

    FIX: security exclude

--- com/src/main/java/Cookease/com/ComApplication.java
+++ com/src/main/java/Cookease/com/ComApplication.java
@@ -2,8 +2,9 @@ package Cookease.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ComApplication {
 
 	public static void main(String[] args) {
