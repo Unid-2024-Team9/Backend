f637f69c104aeaecfe96f75d89430d50735c598b
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-11-03 02:40:32 +0900

    FEAT: cors

--- com/src/main/java/Cookease/com/config/SecurityConfig.java
+++ com/src/main/java/Cookease/com/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configurers.CorsConfig
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -16,11 +18,21 @@ import java.util.Collections;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 설정을 적용
+                .allowedOrigins("*") // 모든 출처 허용
+                .allowedMethods("*") // 모든 HTTP 메서드 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true) // 자격 증명 허용
+                .maxAge(3600); // 캐시 시간 설정 (초 단위)
     }
 
     private static void corsAllow(CorsConfigurer<HttpSecurity> corsCustomizer) {
