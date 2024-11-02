a7a3efa7a294a986a18fc20a1446826ec762b0f3
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-11-03 04:22:38 +0900

    FIX: add http

--- com/src/main/java/Cookease/com/config/SecurityConfig.java
+++ com/src/main/java/Cookease/com/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정을 적용
-                .allowedOrigins("localhost:3000") // 모든 출처 허용
+                .allowedOrigins("http://localhost:3000") // 모든 출처 허용
                 .allowedMethods("*") // 모든 HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 자격 증명 허용
